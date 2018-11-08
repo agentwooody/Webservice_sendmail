eb24e54afe890df4971ffba8de71dafee4866ffe
agentwooody <43959191+agentwooody@users.noreply.github.com>
2018-11-08 16:01:56 +0800

    Add files via upload

---
+++ email/email/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace email
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.emailinterfaceClient s = new ServiceReference1.emailinterfaceClient();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            string payload = textBox2.Text;
+            List<string> URL = new List<string>(a.Split(';'));
+            System.String[] str = URL.ToArray();
+            if (URL.Count > 0)
+                for (int i = 0; i < URL.Count; i++)
+                {
+                    s.SendEmail(str, payload);
+
+                }
+            else {  MessageBox.Show("地址不能为空");
+            }
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
